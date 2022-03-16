72cf4543bb88cf321b7cd8f601b4ebaec0dbedb6
Colin O'Dell <colinodell@gmail.com>
2022-03-16 17:32:09 -0400

    Add copyright docblocks

--- src/Indentation.php
+++ src/Indentation.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the colinodell/indentation package.
+ *
+ * (c) Colin O'Dell <colinodell@gmail.com>
+ *
+ * Forked from detect-indent,
+ * (c) Sindre Sorhus <sindresorhus@gmail.com> (https://sindresorhus.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace ColinODell\Indentation;
 
 final class Indentation
