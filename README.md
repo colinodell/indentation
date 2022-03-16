e07fd63e7a673cbf593da5ddbdbed749a3832512
Colin O'Dell <colinodell@gmail.com>
2022-03-16 15:06:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# indentation
+
+[![Latest Version](https://img.shields.io/packagist/v/colinodell/indentation.svg?style=flat-square)](https://packagist.org/packages/colinodell/indentation)
+[![Total Downloads](https://img.shields.io/packagist/dt/colinodell/indentation.svg?style=flat-square)](https://packagist.org/packages/colinodell/indentation)
+[![Software License](https://img.shields.io/badge/License-MIT-brightgreen.svg?style=flat-square)](LICENSE)
+[![Build Status](https://img.shields.io/github/workflow/status/colinodell/indentation/Tests/main.svg?style=flat-square)](https://github.com/colinodell/indentation/actions?query=workflow%3ATests+branch%3Amain)
+[![Coverage Status](https://img.shields.io/scrutinizer/coverage/g/colinodell/indentation.svg?style=flat-square)](https://scrutinizer-ci.com/g/colinodell/indentation/code-structure)
+[![Quality Score](https://img.shields.io/scrutinizer/g/colinodell/indentation.svg?style=flat-square)](https://scrutinizer-ci.com/g/colinodell/indentation)
+[![Psalm Type Coverage](https://shepherd.dev/github/colinodell/indentation/coverage.svg)](https://shepherd.dev/github/colinodell/indentation)
+[![Sponsor development of this project](https://img.shields.io/badge/sponsor%20this%20package-%E2%9D%A4-ff69b4.svg?style=flat-square)](https://www.colinodell.com/sponsor)
+
+**PHP library to detect and manipulate the indentation of files and strings**
+
+## Installation
+
+```sh
+composer require --dev colinodell/indentation
+```
+
+## Usage
+
+### Detecting the indentation of a string or file
+
+```php
+use ColinODell\Indentation\Indentation;
+
+$indentation = Indentation::detect(file_get_contents('composer.json'));
+
+assert($indentation->getAmount() === 4);
+assert($indentation->getType() === Indentation::TYPE_SPACE);
+assert((string)$indentation === '    ');
+```
+
+### Changing the indentation of a string or file
+
+```php
+use ColinODell\Indentation\Indentation;
+
+$composerJson = file_get_contents('composer.json');
+$composerJson = Indentation::change($composerJson, new Indentation(Indentation::TYPE_TAB, 1));
+file_put_contents('composer.json', $composerJson);
+```
---
+++ tests/IndentationTest.php
@@ -0,0 +1,193 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ColinODell\Indentation\Tests;
+
+use ColinODell\Indentation\Indentation;
+use PHPUnit\Framework\TestCase;
+
+final class IndentationTest extends TestCase
+{
+    public function testConstructorAndGetters(): void
+    {
+        $indentation = new Indentation(4, Indentation::TYPE_SPACE);
+
+        self::assertSame(4, $indentation->getAmount());
+        self::assertSame(Indentation::TYPE_SPACE, $indentation->getType());
+        self::assertSame('    ', (string) $indentation);
+
+        $indentation = new Indentation(1, Indentation::TYPE_TAB);
+
+        self::assertSame(1, $indentation->getAmount());
+        self::assertSame(Indentation::TYPE_TAB, $indentation->getType());
+        self::assertSame("\t", (string) $indentation);
+    }
+
+    /**
+     * @dataProvider provideCasesForTestDetect
+     */
+    public function testDetect(string $filename, Indentation $expected, string $asString): void
+    {
+        $actual = Indentation::detect($filename);
+        self::assertEquals($expected, $actual);
+        self::assertSame($asString, (string) $actual);
+    }
+
+    /**
+     * @return iterable<array<mixed>>
+     */
+    public function provideCasesForTestDetect(): iterable
+    {
+        yield 'Detect space indentation' => [
+            $this->loadFixture('space.js'),
+            new Indentation(4, Indentation::TYPE_SPACE),
+            '    ',
+        ];
+
+        yield 'Detect tab indentation' => [
+            $this->loadFixture('tab.js'),
+            new Indentation(1, Indentation::TYPE_TAB),
+            "\t",
+        ];
+
+        yield 'Detect multiple tabs' => [
+            $this->loadFixture('tab-four.js'),
+            new Indentation(4, Indentation::TYPE_TAB),
+            "\t\t\t\t",
+        ];
+
+        yield 'Detect equal tabs and spaces' => [
+            $this->loadFixture('mixed-tab.js'),
+            new Indentation(1, Indentation::TYPE_TAB),
+            "\t",
+        ];
+
+        yield 'Detect indent of a file with mostly spaces' => [
+            $this->loadFixture('mixed-space.js'),
+            new Indentation(4, Indentation::TYPE_SPACE),
+            '    ',
+        ];
+
+        yield 'Detect indent of a weirdly indented vendor prefixed CSS' => [
+            $this->loadFixture('vendor-prefixed-css.css'),
+            new Indentation(4, Indentation::TYPE_SPACE),
+            '    ',
+        ];
+
+        yield 'Return 0 when these is no indentation' => [
+            '<ul></ul>',
+            new Indentation(0, Indentation::TYPE_UNKNOWN),
+            '',
+        ];
+
+        yield 'Indentation for fifty-fifty indented files with spaces first' => [
+            $this->loadFixture('fifty-fifty-space-first.js'),
+            new Indentation(4, Indentation::TYPE_SPACE),
+            '    ',
+        ];
+
+        yield 'Indentation for fifty-fifty indented files with tabs first' => [
+            $this->loadFixture('fifty-fifty-tab-first.js'),
+            new Indentation(1, Indentation::TYPE_TAB),
+            "\t",
+        ];
+
+        yield 'Indentation for files with spaces and tabs last' => [
+            $this->loadFixture('space-tab-last.js'),
+            new Indentation(1, Indentation::TYPE_TAB),
+            "\t",
+        ];
+
+        yield 'Indentation of a file with single line comments' => [
+            $this->loadFixture('single-space-ignore.js'),
+            new Indentation(4, Indentation::TYPE_SPACE),
+            '    ',
+        ];
+
+        yield 'Indentation for files with single spaces only' => [
+            $this->loadFixture('single-space-only.js'),
+            new Indentation(1, Indentation::TYPE_SPACE),
+            ' ',
+        ];
+    }
+
+    /**
+     * @dataProvider provideCasesForTestChange
+     */
+    public function testChange(string $contents, Indentation $indentation, string $expected): void
+    {
+        $actual = Indentation::change($contents, $indentation);
+        self::assertSame($expected, $actual);
+    }
+
+    /**
+     * @return iterable<array<mixed>>
+     */
+    public function provideCasesForTestChange(): iterable
+    {
+        yield 'Empty string' => [
+            '',
+            new Indentation(4, Indentation::TYPE_SPACE),
+            '',
+        ];
+
+        yield 'No indentation' => [
+            '<ul></ul>',
+            new Indentation(4, Indentation::TYPE_SPACE),
+            '<ul></ul>',
+        ];
+
+        yield 'Two spaces to four spaces' => [
+            "<div>\n  <ul>\n    <li>yay</li>\n  </ul>\n</div>",
+            new Indentation(4, Indentation::TYPE_SPACE),
+            "<div>\n    <ul>\n        <li>yay</li>\n    </ul>\n</div>",
+        ];
+
+        yield 'Four spaces to two spaces' => [
+            "<div>\n    <ul>\n        <li>yay</li>\n    </ul>\n</div>",
+            new Indentation(2, Indentation::TYPE_SPACE),
+            "<div>\n  <ul>\n    <li>yay</li>\n  </ul>\n</div>",
+        ];
+
+        yield 'Two spaces to tabs' => [
+            "<div>\n  <ul>\n    <li>yay</li>\n  </ul>\n</div>",
+            new Indentation(1, Indentation::TYPE_TAB),
+            "<div>\n\t<ul>\n\t\t<li>yay</li>\n\t</ul>\n</div>",
+        ];
+
+        yield 'Four spaces to tabs' => [
+            "<div>\n    <ul>\n        <li>yay</li>\n    </ul>\n</div>",
+            new Indentation(1, Indentation::TYPE_TAB),
+            "<div>\n\t<ul>\n\t\t<li>yay</li>\n\t</ul>\n</div>",
+        ];
+
+        yield 'Tabs to four spaces' => [
+            "<div>\n\t<ul>\n\t\t<li>yay</li>\n\t</ul>\n</div>",
+            new Indentation(4, Indentation::TYPE_SPACE),
+            "<div>\n    <ul>\n        <li>yay</li>\n    </ul>\n</div>",
+        ];
+
+        yield 'Two tabs to two spaces' => [
+            "<div>\n\t\t<ul>\n\t\t\t\t<li>yay</li>\n\t\t</ul>\n</div>",
+            new Indentation(2, Indentation::TYPE_SPACE),
+            "<div>\n  <ul>\n    <li>yay</li>\n  </ul>\n</div>",
+        ];
+
+        yield 'Newlines are preserved' => [
+            "\n<div>\n  <ul>\r\n    <li>yay</li>\r  </ul>\n</div>\n\n",
+            new Indentation(4, Indentation::TYPE_SPACE),
+            "\n<div>\n    <ul>\r\n        <li>yay</li>\r    </ul>\n</div>\n\n",
+        ];
+    }
+
+    private function loadFixture(string $filename): string
+    {
+        $fixture = \file_get_contents(__DIR__ . '/fixtures/' . $filename);
+        if ($fixture === false) {
+            $this->fail('Fixture file not found');
+        }
+
+        return $fixture;
+    }
+}
